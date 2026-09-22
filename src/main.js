7ebc304f1ad7b396fc4f90aaf4d31a83440570ae
BSM1973 <didguitare@me.com>
2026-09-22 16:34:56 +0200

    Load guitar samples through Electron IPC

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
 const path = require('path');
+
+ipcMain.handle('load-guitar-sample', async (_event, fileName) => {
+  const allowed = new Set(['E aigue0.aiff','B0.aiff','G0.aiff','D0.aiff','A0.aiff','E0.aiff']);
+  if (!allowed.has(fileName)) throw new Error('Invalid guitar sample');
+  return fs.promises.readFile(path.join(__dirname, '..', 'assets', 'guitar', 'clean', fileName));
+});
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -9,7 +16,7 @@ function createWindow() {
     minHeight: 700,
     backgroundColor: '#101216',
     title: 'Guitar Liberty',
-    webPreferences: { contextIsolation: true }
+    webPreferences: { contextIsolation: true, preload: path.join(__dirname, 'preload.js') }
   });
   win.loadFile(path.join(__dirname, 'index.html'));
 }
