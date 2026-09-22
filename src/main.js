268f70fcd091a38a572d3583dc1ab9eed8cc08a3
BSM1973 <didguitare@me.com>
2026-09-22 13:48:38 +0200

    Add src/main.js

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1100,
+    minHeight: 700,
+    backgroundColor: '#101216',
+    title: 'Guitar Liberty',
+    webPreferences: { contextIsolation: true }
+  });
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => BrowserWindow.getAllWindows().length === 0 && createWindow());
+});
+app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
