62737dab7479fbcbd2220eac0f6fc699aa234404
BSM1973 <didguitare@me.com>
2026-09-23 09:56:49 +0200

    Expose binary score reader to renderer

--- src/preload.js
+++ src/preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('guitarAudio', {
   loadSample: (fileName) => ipcRenderer.invoke('load-guitar-sample', fileName),
-  importScore: () => ipcRenderer.invoke('import-score')
+  importScore: () => ipcRenderer.invoke('import-score'),
+  readScore: (filePath) => ipcRenderer.invoke('read-score', filePath)
 });
